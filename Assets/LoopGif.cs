28edbffdeeb6e185d094bbc1c6eadc1e57157b4a
epoxy <palmqvist.anton@gmail.com>
2016-10-24 01:14:39 +0200

    added looping sequence of picture frames as animation substitute

---
+++ Assets/LoopGif.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoopGif : MonoBehaviour {
+    private Renderer renderer;
+    public Texture[] frames = null;
+    private int framesPerSecond = 10;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Gif Start");
+        renderer = GetComponent<Renderer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Iterate over the frames
+        int index = (int)(Time.time * framesPerSecond) % frames.Length;
+        renderer.material.mainTexture = frames[index];
+    }
+}
